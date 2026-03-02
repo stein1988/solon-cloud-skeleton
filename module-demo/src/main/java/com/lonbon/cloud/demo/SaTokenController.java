e8518308b46a30f0ea0db858bf06e8874dcabba7
lium <stein1988>
2026-03-02 16:06:19 +0800

    feat(认证): 添加sa-token认证功能
    - 在pom.xml中添加sa-token-solon-plugin依赖
    - 新增SaTokenController实现登录和认证状态检查

---
+++ module-demo/src/main/java/com/lonbon/cloud/demo/SaTokenController.java
@@ -0,0 +1,26 @@
+package com.lonbon.cloud.demo;
+
+import cn.dev33.satoken.stp.StpUtil;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+
+@Controller
+public class SaTokenController {
+
+    // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
+    @Mapping("doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
+    @Mapping("isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
+    }
+}
